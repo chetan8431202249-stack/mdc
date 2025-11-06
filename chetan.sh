c8ee8f2799738480d3fd599fe07c161823fbcbab
root <root@ip-172-31-14-179.ap-south-1.compute.internal>
2025-11-06 06:44:16 +0000

    what a file

---
+++ chetan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shell script to add two numbers
+
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+sum=$((num1 + num2))
+
+echo "The sum of $num1 and $num2 is: $sum"
+echo "i can do it"
